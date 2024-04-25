6bb9fb4e36b6c1b57cfd0f48a193768e2108c48d
YzaatKadyrkulov <yzaatkadyrkulov7@gmail.com>
2024-04-25 16:07:57 +0600

    Initial commit

---
+++ src/main/java/hybernate/App.java
@@ -0,0 +1,13 @@
+package hybernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
